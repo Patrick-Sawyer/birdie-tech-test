a05fa0eced0f39762807b6c3ffa5ce96a2c89ba7
Patrick Sawyer <patrick_sawyer@hotmail.co.uk>
2021-04-26 10:04:09 +0100

    Page number and type filter working on backend

--- front-end/src/components/app/App.tsx
+++ front-end/src/components/app/App.tsx
@@ -3,19 +3,21 @@ import styled, { createGlobalStyle } from 'styled-components';
 import { RootState } from '@App/store/reducers';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-
 import Title from '@App/components/Title';
 import Logo from '@App/components/Logo';
 import SubTitle from '@App/components/SubTitle';
 
 const LogoUrl = require('../../assets/images/logo-birdie.svg');
 
+import fetch from 'node-fetch';
+
 interface AppProps {
 
 }
 
 interface AppState {
-
+  careRecipientId: string;
+  data?: Array<JSON>;
 }
 
 const GlobalStyle = createGlobalStyle`
@@ -40,6 +42,28 @@ const AppContainer = styled.div`
 class App extends React.Component<AppProps, AppState> {
   public constructor(props: AppProps) {
     super(props);
+    this.state = {
+      careRecipientId: 'df50cac5-293c-490d-a06c-ee26796f850d'
+    };
+  }
+
+
+  toJson = (response: Response):PromiseLike<object> => (
+    response.json()
+  );
+
+  getRecipientData = (recipient: string): Promise<object> => {
+    return fetch('http://localhost:8000/observations/recipient?recipient=' + recipient)
+      .then(this.toJson)
+      .then((data: Array<JSON>) => {
+        this.setState({
+          data: data
+        })
+      })
+  };
+
+  componentDidMount = () : void => {
+    this.getRecipientData(this.state.careRecipientId);
   }
 
   public render() {
